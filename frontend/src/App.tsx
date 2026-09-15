3577d46cd9b5dabf1f9b8aa9965dbe45fe431426
yasas s ashinsana <yasasashinsana@gmail.com>
2026-09-15 10:45:43 +0530

    Add pkg to nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -58,6 +58,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
 
         <Route path="/destinations" element={<DestinationListPage />} />
+        <Route path="/packages/new" element={<CreatePackagePage />} />
         <Route path="/packages" element={<PackageListPage />} />
         <Route path="/packages/:id" element={<PackageDetailPage />} />
         <Route path="/accommodations" element={<AccommodationListPage />} />
